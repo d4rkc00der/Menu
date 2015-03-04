2b87439c442e541f769c8e6c294e34bbe5210db3
Evgeniy Shumakov <shumakov.e@gmail.com>
2015-03-04 14:11:16 +0300

    Another func

--- src/First.java
+++ src/First.java
@@ -6,7 +6,9 @@ public class First {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("New func");
+		System.out.println("New func");
+		// Another func
+		System.out.println("This is second func");
 	}
 
 }
